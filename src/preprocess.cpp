72890bc018d18452c5f5512311b694ecefcd7466
Akshay Gulabrao <aksgula22@gmail.com>
2025-07-25 17:37:37 -0700

    added vx,vy,vz

--- src/preprocess.cpp
+++ src/preprocess.cpp
@@ -3,6 +3,12 @@
 #define RETURN0     0x00
 #define RETURN0AND1 0x10
 
+Preprocess::Preprocess(): feature_enabled(0), lidar_type(OUST64), blind(0.01), point_filter_num(1)
+{
+  N_SCANS = 6;
+  
+}
+
 void Preprocess::oust64_handler(const sensor_msgs::PointCloud2::ConstPtr &msg)
 {
   pl_surf.clear();
@@ -14,8 +20,60 @@ void Preprocess::oust64_handler(const sensor_msgs::PointCloud2::ConstPtr &msg)
   int plsize = pl_orig.size();
   pl_corn.reserve(plsize);
   pl_surf.reserve(plsize);
+  if (feature_enabled)
+  {
+    for (int i = 0; i < N_SCANS; i++)
+    {
+      pl_buff[i].clear();
+      pl_buff[i].reserve(plsize);
+    }
 
+    for (uint i = 0; i < plsize; i++){
+      double range = pl_orig.points[i].x * pl_orig.points[i].x +
+               pl_orig.points[i].y * pl_orig.points[i].y +
+               pl_orig.points[i].z * pl_orig.points[i].z;
+      if (range < blind * blind) continue;
+      Eigen::Vector3d pt_vec;
+      PointType added_pt;
+      added_pt.x = pl_orig.points[i].x;
+      added_pt.y = pl_orig.points[i].y;
+      added_pt.z = pl_orig.points[i].z;
+      // 57.3 converts radians to degrees
+      double yaw_angle = atan2(added_pt.y, added_pt.x) * 57.3;
+      if (yaw_angle >= 180.0) yaw_angle -= 180;
+      if (yaw_angle <= -180.0) yaw_angle += 180;
+      added_pt.curvature = pl_orig.points[i].t + time_unit_scale;
+
+      if(pl_orig.points[i].ring < N_SCANS)
+      {
+        pl_buff[pl_orig.points[i].ring].push_back(added_pt);
+      }
+
+    }
+      for (int j = 0; j < N_SCANS; j++){
+        PointCloudXYZI &pl = pl_buff[j];
+        int linesize = pl.size();
+        std::vector<orgtype> &types = types[j];
+        types.clear();
+        types.resize(linesize);
+        linesize--;
+        for (uint i = 0; i < linesize; i++){
+          types[i].range = sqrt(pl[i].x * pl[i].x + pl[i].y * pl[i].y);
+          vx = pl[i].x - pl[i+1].x;
+          vy = pl[i].y - pl[i+1].y;
+          vz = pl[i].z - pl[i+1].z;
+          types[i].dista = vx*vx + vy*vy + vz*vz;
+        }
+        types[linesize].range = sqrt(pl[linesize].x * pl[linesize].x + pl[linesize].y * pl[linesize].y);
+        give_feature(pl,types);
+      }
+    }   
+  else
+  {
+
+  }
 }
+
 
 void velodyne_handler(const sensor_msgs::PointCloud2::ConstPtr &msg)
 {
--- src/preprocess.h
+++ src/preprocess.h
@@ -9,6 +9,27 @@ typedef pcl::PointCloud<PointType> PointCloudXYZI;
 
 enum TIME_UNIT{SEC = 0, MS = 1, US = 2, NS = 3};
 enum LID_TYPE{AVIA = 1, VELO16, OUST64, MARSIM};
+enum Feature{Nor, Poss_Plane, Real_Plane, Edge_Jump, Edge_Plane, Wire, ZeroPoint};
+enum Surround{Prev, Next};
+enum E_jump{Nr_nor, Nr_zero, Nr_180, Nr_inf, Nr_blind};
+
+struct orgtype
+{
+    double range;
+    double dista;
+    double angle[2];
+    double intersect;
+    E_jump edj[2];
+    Feature ftype;
+    orgtype()
+    {
+        range = 0;
+        edj[Prev] = Nr_nor;
+        edj[Next] = Nr_nor;
+        ftype = Nor;
+        intersect = 2;
+    }
+};
 
 namespace ouster_ros{
     struct EIGEN_ALIGN16 Point{
@@ -38,18 +59,26 @@ POINT_CLOUD_REGISTER_POINT_STRUCT(ouster_ros::Point,
 class Preprocess
 {
     public:
+    Preprocess();
     PointCloudXYZI pl_full, pl_corn, pl_surf;
-    
+    PointCloudXYZI pl_buff[128];
+    std::vector<orgtype> typess[128];
+
     float time_unit_scale;
     int time_unit;
     int lidar_type;
-
+    bool feature_enabled;
+    double blind;
+    int point_filter_num;
+    double N_SCANS;
 
 
     void process(const sensor_msgs::PointCloud2::ConstPtr &msg, PointCloudXYZI::Ptr &pcl_out);
 
     private:
     void oust64_handler(const sensor_msgs::PointCloud2::ConstPtr &msg);
+    double vx, vy, vz;
+    
 
 
 
